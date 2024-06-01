d3c49b762e2d55bb6b520590cad273195a3186fa
samwanyua <samexample8@gmail.com>
2024-06-01 18:58:33 +0300

    Create model

--- Restful Api/Restful-Api/src/main/java/com/wanyua/Restful/Api/Repository/StudentRepository.java
+++ Restful Api/Restful-Api/src/main/java/com/wanyua/Restful/Api/Repository/StudentRepository.java
@@ -1,4 +1,9 @@
 package com.wanyua.Restful.Api.Repository;
 
-public interface StudentRepository {
+import com.wanyua.Restful.Api.Models.Students;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Students, Integer> { // Students is the Model class and Integer is the Primary key type
 }
