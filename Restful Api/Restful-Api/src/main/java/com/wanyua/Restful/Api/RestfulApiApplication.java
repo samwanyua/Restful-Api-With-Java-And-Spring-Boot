a34d5e57579470498724b19d3f966ffb110f9287
samwanyua <samexample8@gmail.com>
2024-06-01 13:16:00 +0300

    Initial commit

---
+++ Restful Api/Restful-Api/src/main/java/com/wanyua/Restful/Api/RestfulApiApplication.java
@@ -0,0 +1,13 @@
+package com.wanyua.Restful.Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApiApplication.class, args);
+	}
+
+}
