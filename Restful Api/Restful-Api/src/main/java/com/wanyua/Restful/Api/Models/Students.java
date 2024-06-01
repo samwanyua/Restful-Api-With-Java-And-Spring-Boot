d24727184651671017ac9c5c449da76d519bc973
samwanyua <samexample8@gmail.com>
2024-06-01 18:56:05 +0300

    Create model

--- Restful Api/Restful-Api/src/main/java/com/wanyua/Restful/Api/Models/Students.java
+++ Restful Api/Restful-Api/src/main/java/com/wanyua/Restful/Api/Models/Students.java
@@ -1,4 +1,48 @@
 package com.wanyua.Restful.Api.Models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Students {
+    @Id // To make ID primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // To make ID auto incremented
+    private int id;
+    private String name;
+    private String address;
+
+    public Students() {
+    }
+
+    public Students(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
